6418c25f585ffbb9834cc0aa27b124cf27314f33
Eduard Musatov <pkboyar09@gmail.com>
2025-03-09 18:35:50 +0300

    start using trpc on backend

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,1 +1,16 @@
-console.log('hello');
+import express from 'express';
+import * as trpcExpress from '@trpc/server/adapters/express';
+import { trpcRouter } from './trpc';
+
+const expressApp = express();
+
+expressApp.use(
+  '/trpc',
+  trpcExpress.createExpressMiddleware({
+    router: trpcRouter,
+  })
+);
+
+expressApp.listen(3000, () => {
+  console.log('Listening at http://localhost:3000');
+});
