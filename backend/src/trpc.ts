3eeef1ccad3088bd7e1fa60657846741969b3d02
Eduard Musatov <pkboyar09@gmail.com>
2025-03-11 22:47:05 +0300

    Completed 6 lesson

--- backend/src/trpc.ts
+++ backend/src/trpc.ts
@@ -1,39 +1,32 @@
 import { initTRPC } from '@trpc/server';
+import { times, pick } from 'lodash';
+import { z } from 'zod';
 
-const ideas = [
-  {
-    nick: 'cool-idea-nick-1',
-    name: 'Idea 1',
-    description: 'Description of idea 1...',
-  },
-  {
-    nick: 'cool-idea-nick-2',
-    name: 'Idea 2',
-    description: 'Description of idea 2...',
-  },
-  {
-    nick: 'cool-idea-nick-3',
-    name: 'Idea 3',
-    description: 'Description of idea 3...',
-  },
-  {
-    nick: 'cool-idea-nick-4',
-    name: 'Idea 4',
-    description: 'Description of idea 4...',
-  },
-  {
-    nick: 'cool-idea-nick-5',
-    name: 'Idea 5',
-    description: 'Description of idea 5...',
-  },
-];
+const ideas = times(100, (i) => ({
+  nick: `cool-idea-nick-${i}`,
+  name: `Idea ${i}`,
+  description: `Description of idea ${i}...`,
+  text: times(100, (j) => `<p>Text paragrph ${j} of idea ${i}...</p>`).join(''),
+}));
 
 const trpc = initTRPC.create();
 
 export const trpcRouter = trpc.router({
   getIdeas: trpc.procedure.query(() => {
-    return { ideas };
+    return {
+      ideas: ideas.map((idea) => pick(idea, ['nick', 'name', 'description'])),
+    };
   }),
+  getIdea: trpc.procedure
+    .input(
+      z.object({
+        ideaNick: z.string(),
+      })
+    )
+    .query(({ input }) => {
+      const idea = ideas.find((idea) => idea.nick === input.ideaNick);
+      return { idea: idea || null };
+    }),
 });
 
 export type TrpcRouter = typeof trpcRouter;
