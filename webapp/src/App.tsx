97ad4e1bc0e36681914f62a3b87ef30abbdf12aa
Eduard Musatov <pkboyar09@gmail.com>
2025-03-11 22:07:12 +0300

    Add routing

--- webapp/src/App.tsx
+++ webapp/src/App.tsx
@@ -1,10 +1,25 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import {
+  getAllIdeasRoute,
+  getViewIdeaRoute,
+  viewIdeaRouteParams,
+} from './lib/routes';
 import { TrpcProvider } from './lib/trpc';
 import { AllIdeasPage } from './pages/AllIdeasPage';
+import { ViewIdeaPage } from './pages/ViewIdeaPage';
 
 export const App = () => {
   return (
     <TrpcProvider>
-      <AllIdeasPage />
+      <BrowserRouter>
+        <Routes>
+          <Route path={getAllIdeasRoute()} element={<AllIdeasPage />} />
+          <Route
+            path={getViewIdeaRoute(viewIdeaRouteParams)}
+            element={<ViewIdeaPage />}
+          />
+        </Routes>
+      </BrowserRouter>
     </TrpcProvider>
   );
 };
---
+++ webapp/src/pages/ViewIdeaPage/index.tsx
@@ -0,0 +1,17 @@
+import { useParams } from 'react-router-dom';
+import { type ViewIdeaRouteParams } from '../../lib/routes';
+
+export const ViewIdeaPage = () => {
+  const { ideaNick } = useParams() as ViewIdeaRouteParams;
+  return (
+    <div>
+      <h1>{ideaNick}</h1>
+      <p>Description of idea 1...</p>
+      <div>
+        <p>Text paragrph 1 of idea 1...</p>
+        <p>Text paragrph 2 of idea 1...</p>
+        <p>Text paragrph 3 of idea 1...</p>
+      </div>
+    </div>
+  );
+};
