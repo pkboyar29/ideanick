9259de0f1c39dee035f244185bff31fb388b8d2b
Eduard Musatov <pkboyar09@gmail.com>
2025-03-12 19:26:29 +0300

    Complete the 8 part

---
+++ webapp/src/components/Input/index.tsx
@@ -0,0 +1,33 @@
+import { FormikProps } from 'formik';
+
+export const Input = ({
+  name,
+  label,
+  formik,
+}: {
+  name: string;
+  label: string;
+  formik: FormikProps<any>;
+}) => {
+  const value = formik.values[name];
+  const error = formik.errors[name] as string | undefined;
+  const touched = formik.touched[name];
+
+  return (
+    <div style={{ marginBottom: 10 }}>
+      <label htmlFor={name}>{label}</label>
+      <br />
+      <input
+        type="text"
+        onChange={(e) => {
+          formik.setFieldValue(name, e.target.value);
+        }}
+        onBlur={() => formik.setFieldTouched(name)}
+        value={value}
+        name={name}
+        id={name}
+      />
+      {touched && error && <div style={{ color: 'red' }}>{error}</div>}
+    </div>
+  );
+};
--- webapp/src/pages/NewIdeaPage/index.tsx
+++ webapp/src/pages/NewIdeaPage/index.tsx
@@ -1,5 +1,56 @@
+import { useFormik } from 'formik';
+import { withZodSchema } from 'formik-validator-zod';
+import { z } from 'zod';
+
 import { Segment } from '../../components/Segment';
+import { Input } from '../../components/Input';
+import { Textarea } from '../../components/Textarea';
 
 export const NewIdeaPage = () => {
-  return <Segment title="New Idea">Form will be here...</Segment>;
+  const formik = useFormik({
+    initialValues: {
+      name: '',
+      nick: '',
+      description: '',
+      text: '',
+    },
+    validate: withZodSchema(
+      z.object({
+        name: z.string().min(1),
+        nick: z
+          .string()
+          .min(1)
+          .regex(
+            /^[a-z0-9-]+$/,
+            'Nick may contain only lowercase letters, numbers and dashes'
+          ),
+        description: z.string().min(1),
+        text: z.string().min(100),
+      })
+    ),
+    onSubmit: (values) => {
+      console.log('Submitted', values);
+    },
+  });
+
+  return (
+    <Segment title="New Idea">
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          formik.handleSubmit();
+        }}
+      >
+        <Input name="name" label="Name" formik={formik} />
+        <Input name="nick" label="Nick" formik={formik} />
+        <Input name="description" label="Description" formik={formik} />
+        <Textarea name="text" label="Text" formik={formik} />
+
+        {!formik.isValid && !!formik.submitCount && (
+          <div style={{ color: 'red' }}>Some fields are invalid</div>
+        )}
+        <button type="submit">Create Idea</button>
+      </form>
+    </Segment>
+  );
 };
