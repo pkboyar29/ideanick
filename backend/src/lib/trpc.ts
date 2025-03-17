9937f21a185d1cf32a99ae729a7e2667a60d6fab
Eduard Musatov <pkboyar09@gmail.com>
2025-03-17 22:42:54 +0300

    Add sign up trpc endpoint

--- backend/src/lib/trpc.ts
+++ backend/src/lib/trpc.ts
@@ -4,12 +4,13 @@ import { type Express } from 'express';
 import { type TrpcRouter } from '../router';
 import { type AppContext } from './ctx';
 import superjson from 'superjson';
+import { expressHandler } from 'trpc-playground/handlers/express';
 
 export const trpc = initTRPC.context<AppContext>().create({
   transformer: superjson,
 });
 
-export const applyTrpcToExpressApp = (
+export const applyTrpcToExpressApp = async (
   expressApp: Express,
   appContext: AppContext,
   trpcRouter: TrpcRouter
@@ -19,6 +20,18 @@ export const applyTrpcToExpressApp = (
     trpcExpress.createExpressMiddleware({
       router: trpcRouter,
       createContext: () => appContext,
+    })
+  );
+
+  expressApp.use(
+    '/trpc-playground',
+    await expressHandler({
+      trpcApiEndpoint: '/trpc',
+      playgroundEndpoint: '/trpc-playground',
+      router: trpcRouter,
+      request: {
+        superjson: true,
+      },
     })
   );
 };
--- backend/src/router/index.ts
+++ backend/src/router/index.ts
@@ -3,11 +3,13 @@ import { trpc } from '../lib/trpc';
 import { getIdeaTrpcRoute } from './getIdea';
 import { getIdeasTrpcRoute } from './getIdeas';
 import { createIdeaTrpcRoute } from './createIdea';
+import { signUpTrpcRoute } from './signUp';
 
 export const trpcRouter = trpc.router({
   getIdea: getIdeaTrpcRoute,
   getIdeas: getIdeasTrpcRoute,
   createIdea: createIdeaTrpcRoute,
+  signUp: signUpTrpcRoute,
 });
 
 export type TrpcRouter = typeof trpcRouter;
